848d09280027fea7086fa34118a7cd7134f8c599
PARKSANG\paksa <paksang4@gmail.com>
2024-05-24 21:48:32 +0900

    카트, 찜하기 DTO, VO 구현

---
+++ merry/src/main/java/org/fullstack/merry/domain/ZzimVO.java
@@ -0,0 +1,21 @@
+package org.fullstack.merry.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ZzimVO {
+    private int zzim_idx;
+    private String member_id;
+    private int lec_idx ;
+    private String lec_title;
+    private int lec_price;
+    private String lec_img;
+}
