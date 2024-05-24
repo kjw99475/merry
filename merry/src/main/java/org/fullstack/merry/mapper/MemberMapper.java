cbc61a733f7c018f9a8a76816e5134f8fc69d45b
yunhwa <yun11258@naver.com>
2024-05-24 19:12:55 +0900

    [회원가입] join 기본 기능 완료

--- merry/src/main/java/org/fullstack/merry/mapper/MemberMapper.java
+++ merry/src/main/java/org/fullstack/merry/mapper/MemberMapper.java
@@ -3,7 +3,7 @@ package org.fullstack.merry.mapper;
 import org.fullstack.merry.domain.MemberVO;
 
 public interface MemberMapper {
-//    int join(MemberVO memberVO);
+    int join(MemberVO memberVO);
 //    MemberVO view(String user_id);
 //    int modify(MemberVO memberVO);
 //    int leave(String user_id);
